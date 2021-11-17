ec1d40235529cf126a4fb95c8c32a6d2a2bf85bf
Bruno Vieira <bruno.vieira97@outlook.com>
2021-11-17 03:01:03 -0300

    testes de integração

--- src/test/java/br/com/sicredi/votacao/VotacaoApplicationTests.java
+++ src/test/java/br/com/sicredi/votacao/VotacaoApplicationTests.java
@@ -2,8 +2,9 @@ package br.com.sicredi.votacao;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 class VotacaoApplicationTests {
 
 	@Test
---
+++ src/test/java/br/com/sicredi/votacao/integration/PautaIntegrationTest.java
@@ -0,0 +1,232 @@
+package br.com.sicredi.votacao.integration;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpStatus;
+
+import br.com.sicredi.votacao.dto.PautaRequestDTO;
+import br.com.sicredi.votacao.dto.PautaResponseDTO;
+import io.restassured.http.ContentType;
+
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+public class PautaIntegrationTest {
+
+	@Test
+	public void listaPautas() {
+
+		List<PautaResponseDTO> pautas = 
+			given()
+				.basePath("/pautas")
+				.accept(ContentType.JSON)
+			.when()
+				.get()
+			.then()
+				.statusCode(HttpStatus.OK.value())
+				.extract().body().jsonPath().getList("", PautaResponseDTO.class);
+
+		assertEquals(false, pautas.isEmpty());
+	}
+
+	@Test
+	public void buscaPautaInexistente() {
+		Long id = 5L;
+
+		given()
+			.basePath("/pautas")
+			.pathParam("id", id)
+			.accept(ContentType.JSON)
+		.when()
+			.get("/{id}")
+		.then()
+			.statusCode(HttpStatus.NO_CONTENT.value());
+	}
+
+	@Test
+	public void buscaPautaExistente() {
+		Long id = 1L;
+
+		PautaResponseDTO pauta = 
+			given()
+				.basePath("/pautas")
+				.pathParam("id", id)
+				.accept(ContentType.JSON)
+			.when()
+				.get("/{id}")
+			.then()
+				.statusCode(HttpStatus.OK.value())
+				.extract().body().as(PautaResponseDTO.class);
+
+		assertNotNull(pauta.getId());
+		assertEquals(id, pauta.getId());
+	}
+
+	@Test
+	public void cadastraPautaDadosValidos() {
+		PautaRequestDTO pauta = new PautaRequestDTO();
+		pauta.setNome("Teste");
+
+		Long id = 
+			given()
+				.basePath("/pautas")
+				.accept(ContentType.JSON)
+				.contentType(ContentType.JSON)
+				.body(pauta)
+			.when()
+				.post()
+			.then()
+				.statusCode(HttpStatus.CREATED.value())
+				.extract().body().as(PautaResponseDTO.class).getId();
+
+		given()
+			.basePath("/pautas")
+			.accept(ContentType.JSON)
+			.contentType(ContentType.JSON)
+			.pathParam("id", id)
+		.when()
+			.get("/{id}")
+		.then()
+			.statusCode(HttpStatus.OK.value());
+	}
+
+	@Test
+	public void cadastraPautaDadosInvalidos() {
+		PautaRequestDTO pauta = new PautaRequestDTO();
+		pauta.setNome(null);
+
+		given()
+			.basePath("/pautas")
+			.accept(ContentType.JSON)
+			.contentType(ContentType.JSON)
+			.body(pauta)
+		.when()
+			.post()
+		.then()
+			.statusCode(HttpStatus.BAD_REQUEST.value());
+	}
+
+	@Test
+	public void atualizaPautaDadosValidos() {
+		Long id = 1L;
+		String novoNome = "Novo nome";
+
+		PautaResponseDTO pauta = 
+			given()
+				.basePath("/pautas")
+				.pathParam("id", id)
+				.accept(ContentType.JSON)
+			.when()
+				.get("/{id}")
+			.then()
+				.statusCode(HttpStatus.OK.value())
+				.extract().body().as(PautaResponseDTO.class);
+
+		assertEquals(id, pauta.getId());
+
+		PautaRequestDTO newPauta = new PautaRequestDTO();
+		newPauta.setNome(novoNome);
+
+		PautaResponseDTO response =
+			given()
+				.basePath("/pautas")
+				.accept(ContentType.JSON)
+				.contentType(ContentType.JSON)
+				.pathParam("id", id)
+				.body(newPauta)
+			.when()
+				.put("/{id}")
+			.then()
+				.statusCode(HttpStatus.OK.value())
+				.extract().body().as(PautaResponseDTO.class);
+		
+		assertEquals(novoNome, response.getNome());
+	}
+
+	@Test
+	public void atualizaPautaDadosInvalidos() {
+		Long id = 1L;
+		String novoNome = null;
+
+		PautaResponseDTO pauta = 
+			given()
+				.basePath("/pautas")
+				.accept(ContentType.JSON)
+				.pathParam("id", id)
+			.when()
+				.get("/{id}")
+			.then()
+				.statusCode(HttpStatus.OK.value())
+				.extract().body().as(PautaResponseDTO.class);
+
+		assertEquals(id, pauta.getId());
+
+		PautaRequestDTO newPauta = new PautaRequestDTO();
+		newPauta.setNome(novoNome);
+
+		given()
+			.basePath("/pautas")
+			.accept(ContentType.JSON)
+			.contentType(ContentType.JSON)
+			.pathParam("id", id)
+			.body(newPauta)
+		.when()
+			.put("/{id}")
+		.then()
+			.statusCode(HttpStatus.BAD_REQUEST.value());
+	}
+
+	@Test
+	public void excluiPautaInexistente() {
+		Long id = 10L;
+
+		given()
+			.basePath("/pautas")
+			.accept(ContentType.JSON)
+			.contentType(ContentType.JSON)
+			.pathParam("id", id)
+		.when()
+			.delete("/{id}")
+		.then()
+			.statusCode(HttpStatus.NO_CONTENT.value());
+
+		given()
+			.basePath("/pautas")
+			.accept(ContentType.JSON)
+			.pathParam("id", id)
+		.when()
+			.get("/{id}")
+		.then()
+			.statusCode(HttpStatus.NO_CONTENT.value());
+	}
+
+	@Test
+	public void excluiPautaExistente() {
+		Long id = 1L;
+
+		given()
+			.basePath("/pautas")
+			.accept(ContentType.JSON)
+			.contentType(ContentType.JSON)
+			.pathParam("id", id)
+		.when()
+			.delete("/{id}")
+		.then()
+			.statusCode(HttpStatus.OK.value());
+
+		given()
+			.basePath("/pautas")
+			.accept(ContentType.JSON)
+			.contentType(ContentType.JSON)
+			.pathParam("id", id)
+		.when()
+			.get("/{id}")
+		.then()
+			.statusCode(HttpStatus.NO_CONTENT.value());
+	}
+}
