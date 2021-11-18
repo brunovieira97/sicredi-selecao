8856ea1202bff39f65dd44fc602c40a58f7f80e3
Bruno Vieira <bruno.vieira97@outlook.com>
2021-11-18 02:37:43 -0300

    setup de bd para sessão

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,4 @@
 insert into pauta (nome) values ('Utilização de Java');
 insert into pauta (nome) values ('Utilização de React');
+insert into sessao (data_hora_abertura, data_hora_fechamento, pauta_id) values ('2021-12-10T08:00:00', '2021-12-12T08:00:00', 1);
+insert into sessao (data_hora_abertura, data_hora_fechamento, pauta_id) values ('2022-12-10T08:00:00', '2023-12-10T08:00:00', 2);
