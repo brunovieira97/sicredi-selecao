de0fe9126d737767080a9d87cfcf8c3484f643af
Bruno Vieira <bruno.vieira97@outlook.com>
2021-11-18 23:39:11 -0300

    hotfix no valor do voto  - ajuste para salvar o ordinal do enum ao invés de S e N

--- src/main/java/br/com/sicredi/votacao/enumerator/ValorVoto.java
+++ src/main/java/br/com/sicredi/votacao/enumerator/ValorVoto.java
@@ -5,9 +5,20 @@ import java.util.stream.Stream;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
+/**
+ * ! Problema
+ * 
+ * Infelizmente, devido aos funcionamentos padrões da JPA, persistir a chave (S ou N)
+ * dependeria da implementação de um converter, que não interagiu bem com a transformação
+ * de/para JSON no meu primeiro teste.
+ * 
+ * Vou deixar assim, por enquanto, para buscar uma alternativa.
+ * 
+ * TODO: Salvar "S" ou "N" no banco
+ */
 public enum ValorVoto {
-	SIM('S'),
-	NAO('N');
+	NAO('N'),
+	SIM('S');
 
 	private Character value;
 
