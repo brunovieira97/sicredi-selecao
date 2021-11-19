009d8b52379e81c56e77f5f9d7d90ea6ae951818
Bruno Vieira <bruno.vieira97@outlook.com>
2021-11-18 21:10:24 -0300

    exceptions novas

---
+++ src/main/java/br/com/sicredi/votacao/exception/VotoJaRegistradoException.java
@@ -0,0 +1,9 @@
+package br.com.sicredi.votacao.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class VotoJaRegistradoException extends RuntimeException {
+	
+}
